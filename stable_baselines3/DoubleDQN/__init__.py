00055a27d60a7f4199c8aa5b5fede5ccc6de57d8
awanhuzaifa <163748117+awanhuzaifa@users.noreply.github.com>
2024-03-18 11:06:08 +0500

    Add files via upload

---
+++ stable_baselines3/DoubleDQN/__init__.py
@@ -0,0 +1,4 @@
+from stable_baselines3.dqn.double_q import DoubleDQN
+from stable_baselines3.dqn.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
+
+__all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "DoubleDQN"]
