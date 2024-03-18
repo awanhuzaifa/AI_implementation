e372a28ffc1eb99c599056d9aca6f5439fe4edc4
awanhuzaifa <163748117+awanhuzaifa@users.noreply.github.com>
2024-03-18 02:21:44 -0700

    Update __init__.py

--- stable_baselines3/dqn/__init__.py
+++ stable_baselines3/dqn/__init__.py
@@ -1,5 +1,4 @@
-from stable_baselines3.dqn import DQN
-
+from stable_baselines3.dqn.dqn import DQN
 from stable_baselines3.dqn.policies import CnnPolicy, MlpPolicy, MultiInputPolicy
 
 __all__ = ["CnnPolicy", "MlpPolicy", "MultiInputPolicy", "DQN"]
