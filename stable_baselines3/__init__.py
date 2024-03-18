4d3222ce6e9428640c252a4856ce1697f9c46f10
awanhuzaifa <163748117+awanhuzaifa@users.noreply.github.com>
2024-03-18 11:21:15 +0500

    Update __init__.py

--- stable_baselines3/__init__.py
+++ stable_baselines3/__init__.py
@@ -9,7 +9,7 @@ from stable_baselines3.ppo import PPO
 from stable_baselines3.sac import SAC
 from stable_baselines3.td3 import TD3
 from stable_baselines3.DoubleDQN import DOUBLEDQN
-
+from stable_baselines3.dqn_double_q import DoubleDQN
 
 # Read version from file
 version_file = os.path.join(os.path.dirname(__file__), "version.txt")
